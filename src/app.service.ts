ebca4a214527caed1dcbb61c0127693080e02979
evikadar <evi.kadar@gmail.com>
2023-08-16 10:31:40 +0200

    config file and cleanup

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,19 +1,25 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { AxiosError, AxiosResponse } from 'axios';
+import { AxiosError } from 'axios';
 import { catchError, firstValueFrom } from 'rxjs';
 import { RouteInterface } from './route.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   private readonly logger = new Logger(AppService.name);
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private configService: ConfigService,
+  ) {}
 
   async findDistanceTo(zipCode: string): Promise<RouteInterface> {
+    const googleRouteApiKey =
+      this.configService.get<string>('GOOGLE_ROUTE_API');
     const { data } = await firstValueFrom(
       this.httpService
         .post<RouteInterface>(
-          'https://routes.googleapis.com/directions/v2:computeRoutes?key=AIzaSyBO8TDRlF8lO8MJZ4M8Sm7TwdOf0DpazHM&$fields=routes.distanceMeters',
+          `https://routes.googleapis.com/directions/v2:computeRoutes?key=${googleRouteApiKey}&$fields=routes.distanceMeters`,
           {
             origin: {
               address: '6088 Magyarország',
