2bf346692db9817f68dc21e48fe058db943408b1
evikadar <evi.kadar@gmail.com>
2023-08-23 13:37:16 +0200

    Ignore favicon requests

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Ignore favicon requests
+  app.use((req, res, next) => {
+    if (req.originalUrl === '/favicon.ico') {
+      res.status(204).end();
+    } else {
+      next();
+    }
+  });
+
   await app.listen(3000);
 }
 bootstrap();
