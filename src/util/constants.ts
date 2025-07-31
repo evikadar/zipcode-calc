fc5d063230e17fdbce64b63f1cb17c50ca9bbbe7
grimberden <164159953+grimberden@users.noreply.github.com>
2025-07-31 12:49:29 +0200

    Update constants.ts

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -3,7 +3,7 @@ import { Prices } from 'src/route.interface';
 export const basePricePerKm = 430;
 export const transferPriceFrom121To400 = 725;
 export const transferPriceFrom401To500 = 750;
-export const pricePerPallet = 4000;
+export const pricePerPallet = 5000;
 // the transfer price must be at least this much with and without loading
 export const minTransferPriceWithoutLoading = 20000;
 export const minTransferPriceWithLoading = 30000;
