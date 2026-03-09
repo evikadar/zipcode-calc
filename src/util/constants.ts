bd507b3b3c1c26d219015dfad4a67d5fc1a6bb35
grimberden <164159953+grimberden@users.noreply.github.com>
2026-03-09 12:17:00 +0100

    Update constants.ts

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,8 +1,8 @@
 import { Prices } from 'src/route.interface';
 
-export const basePricePerKm = 430;
-export const transferPriceFrom121To400 = 725;
-export const transferPriceFrom401To500 = 750;
+export const basePricePerKm = 500;
+export const transferPriceFrom121To400 = 800;
+export const transferPriceFrom401To500 = 800;
 export const pricePerPallet = 5000;
 // the transfer price must be at least this much with and without loading
 export const minTransferPriceWithoutLoading = 20000;
@@ -11,18 +11,18 @@ export const minTransferPriceWithLoading = 30000;
 // 1: SPORT, 2: MEDITERRAN, 3: REGEN
 export const prices: Prices = {
   1: {
-    '1-200': 1988,
-    '201-400': 1929,
-    '400+': 1890,
+    '1-200': 2028,
+    '201-400': 1969,
+    '400+': 1941,
   },
   2: {
-    '1-200': 1988,
-    '201-400': 1929,
-    '400+': 1890,
+    '1-200': 2028,
+    '201-400': 1969,
+    '400+': 1941,
   },
   3: {
-    '1-200': 1988,
-    '201-400': 1929,
-    '400+': 1890,
+    '1-200': 2028,
+    '201-400': 1969,
+    '400+': 1941,
   },
 };
